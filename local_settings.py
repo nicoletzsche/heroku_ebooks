91e0d322258ae8e3611981180afd7ef8233481dd
nicoletzsche <99277018+nicoletzsche@users.noreply.github.com>
2022-02-08 11:16:18 -0700

    Update local_settings.py

--- local_settings.py
+++ local_settings.py
@@ -7,7 +7,7 @@ Local Settings for a heroku_ebooks account.
 # Configuration for Twitter API
 ENABLE_TWITTER_SOURCES = True # Fetch twitter statuses as source
 ENABLE_TWITTER_POSTING = True # Tweet resulting status?
-MY_CONSUMER_KEY = environ.get('TUTBRWVVxd0hKM2lwQi1lal9LOVo6MTpjaQ')#Your Twitter API Consumer Key set in Heroku config
+MY_CONSUMER_KEY = environ.get('549f5eaa-92cf-46dc-8c50-bc20e0935ea4')#Your Twitter API Consumer Key set in Heroku config
 MY_CONSUMER_SECRET = environ.get('3dYmKMwGHMJnP1J4yJgKxx6HWoV1GaynQKOBnKKujsfY3ptEBe')#Your Consumer Secret Key set in Heroku config
 MY_ACCESS_TOKEN_KEY = environ.get('1491082167375466496-Lf1jl6iL1k1fVNJvDmtMth8jEb4wkp')#Your Twitter API Access Token Key set in Heroku config
 MY_ACCESS_TOKEN_SECRET = environ.get('4ncp0YCU9tCYaCvY9knzfzunpBozhNQBQKdXJKnMKgIYkFaMzc')#Your Access Token Secret set in Heroku config
